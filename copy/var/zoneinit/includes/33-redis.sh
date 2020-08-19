b99e55aec4114cc761ac209cfc001c06cbfee4eb
jfqd <jfqd@blun.org>
2020-08-19 17:23:45 +0200

    add socket support back

--- copy/var/zoneinit/includes/33-redis.sh
+++ copy/var/zoneinit/includes/33-redis.sh
@@ -63,6 +63,7 @@ gsed -i \
      -e "s/protected-mode yes/protected-mode no/" \
      -e "s/# maxmemory <bytes>/maxmemory 1gb/" \
      -e "s/# maxmemory-policy noeviction/# maxmemory-policy allkeys-lfu/" \
+     -e "s/# unixsocket \/tmp\/redis.sock/unixsocket \/var\/tmp\/redis.sock/" \
      /opt/local/etc/redis.conf
 
 cat >> /opt/local/etc/redis.conf << EOF
